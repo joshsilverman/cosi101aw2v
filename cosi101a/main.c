0c64ce92ff610ede941b0ae5f61a0a91aaa09cc1
Josh Silverman <joshs.silverman@gmail.com>
2015-11-30 17:46:21 -0500

    Initial Commit

---
+++ cosi101a/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cosi101a
+//
+//  Created by Josh Silverman on 11/30/15.
+//  Copyright (c) 2015 JSS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
