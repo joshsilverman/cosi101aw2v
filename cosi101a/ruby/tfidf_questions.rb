e09e1d666c230a5bda8bd0367d092b6a7f4a9386
Josh Silverman <joshs.silverman@gmail.com>
2015-12-12 14:13:10 -0500

    slight improvement to 33.88

--- cosi101a/ruby/tfidf_questions.rb
+++ cosi101a/ruby/tfidf_questions.rb
@@ -2,6 +2,7 @@ require 'csv'
 require 'pry'
 require 'matrix'
 require 'tf-idf-similarity'
+require 'benchmark'
 # require 'narray'
 
 # ['data/training_set', 'data/validation_set'].each do |filename|
@@ -9,6 +10,7 @@ sets = {'data/training_set' => [1,3,4,5,6] }#,
         # 'data/validation_set' => [1,2,3,4,5] }
 
 model = nil
+puts "compiling documents..."
 sets.each do |filename, indices|
   contents = ""
   open(filename + '.tsv', 'r+') do |f|
@@ -27,40 +29,46 @@ sets.each do |filename, indices|
   end
 
   # corpus = corpus[0..100]
-  # model = TfIdfSimilarity::TfIdfModel.new(corpus)
-  model = TfIdfSimilarity::BM25Model.new(corpus)
+  puts "begining model construction..."
+  t = Benchmark.measure { model = TfIdfSimilarity::TfIdfModel.new(corpus) }
+  puts "completed in #{t.real} seconds"
+  puts "begining term scoring..."
+  # model = TfIdfSimilarity::BM25Model.new(corpus)
 
-  CSV.open("#{filename}_tfidf.csv", "wb") do |csv|    
-    CSV.foreach("#{filename}.csv") do |row|
-      indices.each do |i|
-        document = TfIdfSimilarity::Document.new(row[i])
-        tfidf_by_term = {}
-        document.terms.each do |term|
-          tfidf_by_term[term] = model.tfidf(document, term)
-        end
-
-        if tfidf_by_term.values.count == 0
-          next
-        end
-
-        terms = []
-        # max = tfidf_by_term.values.max
-        min = tfidf_by_term.values.min
-        range = max - min
-        # threshold = range / 4.0 + min
-
-        # average = tfidf_by_term.reduce(:+) / tfidf_by_term.size.to_f
-        threshold = [6.5, max].min
-
-        tfidf_by_term.each do |term, score|
-          if score >= threshold 
-            terms << term
+  t = Benchmark.measure do
+    CSV.open("#{filename}_tfidf.csv", "wb") do |csv|    
+      CSV.foreach("#{filename}.csv") do |row|
+        indices.each do |i|
+          document = TfIdfSimilarity::Document.new(row[i])
+          tfidf_by_term = {}
+          document.terms.each do |term|
+            tfidf_by_term[term] = model.tfidf(document, term)
           end
-        end
 
-        row[i] = terms.join " "
+          if tfidf_by_term.values.count == 0
+            next
+          end
+
+          terms = []
+          max = tfidf_by_term.values.max
+          # min = tfidf_by_term.values.min
+          # range = max - min
+          # threshold = range / 4.0 + min
+
+          # average = tfidf_by_term.reduce(:+) / tfidf_by_term.size.to_f
+          threshold = [5.5, max].min
+
+          tfidf_by_term.each do |term, score|
+            if score >= threshold 
+              terms << term
+            end
+          end
+
+          row[i] = terms.join " "
+        end
+        csv << row
       end
-      csv << row
     end
   end
+  puts "completed in #{t.real} seconds"
 end
